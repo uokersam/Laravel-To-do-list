79d9623e649f5003a30daf2ee34b7248a0b81b80
Denis Karakchiev <uokersam@gmail.com>
2018-12-27 19:04:01 +0300

    migration for to-do list table

---
+++ blog/database/migrations/2018_12_27_160103_to_do_list.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ToDoList extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todolist', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('content');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todolist');
+    }
+}
