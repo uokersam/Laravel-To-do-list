0632a15b5c7e85f97e2056d2cda96ae667717bfa
Denis Karakchiev <uokersam@gmail.com>
2018-12-27 19:06:56 +0300

    CRUD for to-do list (starting)

---
+++ blog/app/Http/Controllers/ToDoListController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ToDoListController extends Controller
+{
+    public function create()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+
+
+}
