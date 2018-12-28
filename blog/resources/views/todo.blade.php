e38a541d60302a5df3e2a19fc4c1e27a4310d74d
Denis Karakchiev <uokersam@gmail.com>
2018-12-28 19:04:23 +0300

    todo list page

---
+++ blog/resources/views/todo.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    <hr>
+    <b>To-do list:</b>
+    <ul>
+        @foreach ($items as $item)
+            <li>{{ $item->content }}</li>
+        @endforeach
+    </ul>
+
+    <form action="{{ action('ToDoListController@create') }}" method="post" accept-charset="UTF-8">
+        @csrf
+        <input type="text" placeholder="some text" name="textitem">
+        <button type="submit">Submit</button>
+    </form>
+
+
+@endsection
