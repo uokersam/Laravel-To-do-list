7d9462d2488919231fead6e06cdc2b3830db3629
Denis Karakchiev <uokersam@gmail.com>
2018-12-28 19:25:40 +0300

    templating: helpers

---
+++ blog/resources/views/helpers/menu.blade.php
@@ -0,0 +1,5 @@
+<nav>
+    <a href="/">Home</a>
+    <a href="/todo">To-do list</a>
+
+</nav>
--- blog/resources/views/layouts/app.blade.php
+++ blog/resources/views/layouts/app.blade.php
@@ -1,14 +1,12 @@
+
+
 @section('title', 'App page')
 <!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>App page - @yield('title')</title>
-</head>
+@include('helpers.head')
 <body>
+
+@include('helpers.menu')
 
 @section('sidebar')
     this is template master sidebar
