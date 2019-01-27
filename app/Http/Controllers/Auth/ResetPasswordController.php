7ba6efd5e446dbc0097b23fbdbc89da3caf27c53
Denis Karakchiev <uokersam@gmail.com>
2019-01-27 13:14:27 +0300

    Authentication: starting point

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Auth\Events\PasswordReset;
 
 class ResetPasswordController extends Controller
 {
@@ -19,6 +20,22 @@ class ResetPasswordController extends Controller
     */
 
     use ResetsPasswords;
+
+    /**
+     * Reset the given user's password.
+     *
+     * @param  \Illuminate\Contracts\Auth\CanResetPassword  $user
+     * @param  string $password
+     * @return void
+     */
+    protected function resetPassword($user, $password)
+    {
+        $user->password = $password;
+        $user->setRememberToken(str_random(60));
+        $user->save();
+        event(new PasswordReset($user));
+        $this->guard()->login($user);
+    }
 
     /**
      * Where to redirect users after resetting their password.
